fa377df2e5e8b3e4a753e9bd3079e0f9fd9595c3
kittin03 <katarinam@centrum.cz>
2016-06-16 19:57:08 +0200

    just testing...

---
+++ test.js
@@ -0,0 +1,10 @@
+var blah = "blahblah";
+var x = 1;
+var y = 2;
+console.log(`${blah} 
+	dryrd 
+		tryrdu 
+${x} + ${y} = ${x + y}
+tyhdry`);
+var message = `It's ${new Date().getHours()} and I'm sleepy`;
+console.log(message);
